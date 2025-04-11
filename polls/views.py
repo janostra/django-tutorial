b084914a02386cfd4cdfa5c0824418963282797c
Jano Stratakis <jano.stratakis123@gmail.com>
2025-04-10 21:41:09 -0300

    [Parte 4.2] Crear vistas más complejas y actualizar rutas

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,13 @@
 from django.http import HttpResponse # type: ignore
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("¡Estás viendo el índice de encuestas!")
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}.")
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando en la pregunta {question_id}.")
