e327a588b291f39a937477c85443c12500d723c8
Jano Stratakis <jano.stratakis123@gmail.com>
2025-04-10 21:28:33 -0300

    [Parte 2.1] Crea modelo Question

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,5 @@
-from django.db import models
+from django.db import models # type: ignore
 
-# Create your models here.
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
